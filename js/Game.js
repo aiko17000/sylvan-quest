20073d51a1b68412bc3bd8f1cfbc0c695fe179d4
aiko17000 <54277200+aiko17000@users.noreply.github.com>
2019-08-30 16:01:13 +0200

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -7,14 +7,25 @@ class Game extends EventTarget{
         this.currentState = new WelcomeState(this);
         this.endEvt = new Event('ended');
     }
+    
+    changeState(state)
+    {
+        this.currentState = state;
+        sleep(200).then(() => {
+            this.currentState.go();
+        });
+    }
+    
+    start()
+    {
+        console.log("go");
+        this.currentState.go();
+    }
 }
 
 Game.prototype.changeState = function (state) {
     //if (this.count++ >= 10) return;
-    this.currentState = state;
-    sleep(200).then(() => {
-        this.currentState.go();
-    });
+
 };
 
 // sleep time expects milliseconds
@@ -22,8 +33,3 @@ function sleep (time) {
     return new Promise((resolve) => setTimeout(resolve, time));
   }
 
-
-Game.prototype.start = function () {
-    console.log("go");
-    this.currentState.go();
-};
