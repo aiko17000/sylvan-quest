be41674e2b5489a724863b52cc2798d24d0853a5
Dev <you@example.com>
2019-09-04 12:49:46 +0200

    continue

--- js/state/GameplayState.js
+++ js/state/GameplayState.js
@@ -61,7 +61,7 @@ class GameplayState {
         {
             window.game.currentState.inStep =  true;
             console.log(window.game.test.getStep());
-            window.navigator.vibrate([500]);
+            navigator.vibrate([500]);
             window.game.test.getStep().render();
         }
       }
